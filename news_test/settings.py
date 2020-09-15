4006e9db34532baae001cff124c017c8700068f1
Vladimir <vladimir.dolgov@yahoo.com>
2020-09-16 00:09:11 +0300

    fix

--- news_test/settings.py
+++ news_test/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = "news_test.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, 'post/templates/post')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
