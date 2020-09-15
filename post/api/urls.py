bbc8ddbe60c6210b6bc4ef209a739714d161bb1f
Vladimir <vladimir.dolgov@yahoo.com>
2020-09-15 20:08:30 +0300

    Auto stash before revert of "started working on docker"
    added api authentication

--- post/api/urls.py
+++ post/api/urls.py
@@ -4,10 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import PostList, PostDetail, CommentList, CommentDetail
 
 urlpatterns = [
-    path('posts/', PostList.as_view()),
-    path('posts/<int:pk>/', PostDetail.as_view()),
-    path('comments/', CommentList.as_view()),
-    path('comments/<int:pk>/', CommentDetail.as_view()),
+    path("posts/", PostList.as_view()),
+    path("posts/<int:pk>/", PostDetail.as_view()),
+    path("comments/", CommentList.as_view()),
+    path("comments/<int:pk>/", CommentDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
